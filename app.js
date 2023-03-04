c693a3acd90f8936ab37fa36a56a1ca54a327610
stefanolami <stefanolami90@gmail.com>
2023-03-04 11:55:07 +0100

    second commit

--- app.js
+++ app.js
@@ -3,24 +3,24 @@ const mobileMenuNav = document.getElementById('mobile-menu-nav');
 const mobileMenuClose = document.getElementById('mobile-menu-close');
 const mobileMenuDiv = document.getElementById('mobile-menu-div');
 
-if (window.screen.width < 768) {
-    mobileMenu.addEventListener('click', () => {
-        mobileMenuNav.classList.replace('hidden', 'flex');
-        console.log('opening');
-    })
-    
-    mobileMenuClose.addEventListener('click', () => {
+
+mobileMenu.addEventListener('click', () => {
+    mobileMenuNav.classList.replace('hidden', 'flex');
+    console.log('opening');
+})
+
+mobileMenuClose.addEventListener('click', () => {
+    mobileMenuNav.classList.replace('flex', 'hidden');
+    console.log('closing');
+})
+
+document.addEventListener('click', (event) => {
+    if (!mobileMenuDiv.contains(event.target)) {
         mobileMenuNav.classList.replace('flex', 'hidden');
         console.log('closing');
-    })
-    
-    document.addEventListener('click', (event) => {
-        if (!mobileMenuDiv.contains(event.target)) {
-            mobileMenuNav.classList.replace('flex', 'hidden');
-            console.log('closing');
-            console.log('containbs');
-        }
-    })
-    console.log('listening event')
-}
+        console.log('containbs');
+    }
+})
+console.log('listening event')
+
 
