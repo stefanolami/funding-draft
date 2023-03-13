d39247b004c3d6f00fcd319ecb252fca0316814e
stefanolami <stefanolami90@gmail.com>
2023-03-13 19:52:00 +0100

    assembling landing page

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['./src/html/index.html', './src/html/editing.html'],
+  content: ['./src/html/index.html', './src/html/editing.html', './src/html/home.html'],
   theme: {
     extend: {
       colors: {
