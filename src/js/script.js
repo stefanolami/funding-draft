c557484f2853d7a5dd0977cb138415ad64212eab
stefanolami <stefanolami90@gmail.com>
2023-03-17 15:24:38 +0100

    added all functionalities to mobile our services page

--- src/js/script.js
+++ src/js/script.js
@@ -18,19 +18,31 @@ document.addEventListener('click', (event) => {
     }
 })
 
-/*      OUR SERVICES        */
+/*     MOBILE OUR SERVICES        */
 
-const servicesProjInitDivs = Array.from(document.getElementsByClassName('project-initiation'))
-const servicesProjInitBtns = Array.from(document.getElementsByClassName('project-initiation-btns'))
+const mobileServicesProjInitDivs = Array.from(document.getElementsByClassName('mobile-project-initiation'))
+const mobileServicesProjInitBtns = Array.from(document.getElementsByClassName('mobile-project-initiation-btns'))
+const mobileServicesProjPlanDivs = Array.from(document.getElementsByClassName('mobile-project-planning'))
+const mobileServicesProjPlanBtns = Array.from(document.getElementsByClassName('mobile-project-planning-btns'))
+const mobileServicesProjExecDivs = Array.from(document.getElementsByClassName('mobile-project-execution'))
+const mobileServicesProjExecBtns = Array.from(document.getElementsByClassName('mobile-project-execution-btns'))
+const mobileServicesProjClosDivs = Array.from(document.getElementsByClassName('mobile-project-closure'))
+const mobileServicesProjClosBtns = Array.from(document.getElementsByClassName('mobile-project-closure-btns'))
 
 
+function handleMobileServicesBtns(arr1, arr2, str) {
+    arr1.forEach(btn => btn.addEventListener('click', (e) => {
+        btnIndex = arr1.findIndex(element => element == e.target)
+        btnFocusIndex = arr1.findIndex(element => element.classList.contains(`mobile-project-${str}-focused`))
+        divIndex = arr2.findIndex(element => !element.classList.contains('hidden'))
+        arr1[btnFocusIndex].classList.replace(`mobile-project-${str}-focused`, `mobile-project-${str}-unfocused`)
+        e.target.classList.replace(`mobile-project-${str}-unfocused`, `mobile-project-${str}-focused`)
+        arr2[divIndex].classList.replace('block', 'hidden');
+        arr2[btnIndex].classList.replace('hidden', 'block');
+    }))
+}
 
-servicesProjInitBtns.forEach(btn => btn.addEventListener('click', (e) => {
-    btnIndex = servicesProjInitBtns.findIndex(element => element == e.target)
-    btnFocusIndex = servicesProjInitBtns.findIndex(element => element.classList.contains('project-initiation-focused'))
-    divIndex = servicesProjInitDivs.findIndex(element => !element.classList.contains('hidden'))
-    servicesProjInitBtns[btnFocusIndex].classList.replace('project-initiation-focused', 'project-initiation-unfocused')
-    e.target.classList.replace('project-initiation-unfocused', 'project-initiation-focused')
-    servicesProjInitDivs[divIndex].classList.replace('block', 'hidden');
-    servicesProjInitDivs[btnIndex].classList.replace('hidden', 'block');
-}))
+handleMobileServicesBtns(mobileServicesProjInitBtns, mobileServicesProjInitDivs, 'initiation')
+handleMobileServicesBtns(mobileServicesProjPlanBtns, mobileServicesProjPlanDivs, 'planning')
+handleMobileServicesBtns(mobileServicesProjExecBtns, mobileServicesProjExecDivs, 'execution')
+handleMobileServicesBtns(mobileServicesProjClosBtns, mobileServicesProjClosDivs, 'closure')
