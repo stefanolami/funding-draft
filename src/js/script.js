0a2d6ff3911979b025a437e4fc526d4e9cea3c87
stefanolami <stefanolami90@gmail.com>
2023-03-17 14:53:39 +0100

    done our services mobile section

--- src/js/script.js
+++ src/js/script.js
@@ -6,18 +6,31 @@ const mobileMenuDiv = document.getElementById('mobile-menu-div');
 
 mobileMenu.addEventListener('click', () => {
     mobileMenuNav.classList.replace('hidden', 'flex');
-    console.log('opening');
 })
 
 mobileMenuClose.addEventListener('click', () => {
     mobileMenuNav.classList.replace('flex', 'hidden');
-    console.log('closing');
 })
 
 document.addEventListener('click', (event) => {
     if (!mobileMenuDiv.contains(event.target)) {
         mobileMenuNav.classList.replace('flex', 'hidden');
-        console.log('closing');
-        console.log('contains');
     }
 })
+
+/*      OUR SERVICES        */
+
+const servicesProjInitDivs = Array.from(document.getElementsByClassName('project-initiation'))
+const servicesProjInitBtns = Array.from(document.getElementsByClassName('project-initiation-btns'))
+
+
+
+servicesProjInitBtns.forEach(btn => btn.addEventListener('click', (e) => {
+    btnIndex = servicesProjInitBtns.findIndex(element => element == e.target)
+    btnFocusIndex = servicesProjInitBtns.findIndex(element => element.classList.contains('project-initiation-focused'))
+    divIndex = servicesProjInitDivs.findIndex(element => !element.classList.contains('hidden'))
+    servicesProjInitBtns[btnFocusIndex].classList.replace('project-initiation-focused', 'project-initiation-unfocused')
+    e.target.classList.replace('project-initiation-unfocused', 'project-initiation-focused')
+    servicesProjInitDivs[divIndex].classList.replace('block', 'hidden');
+    servicesProjInitDivs[btnIndex].classList.replace('hidden', 'block');
+}))
--- tailwind.config.js
+++ tailwind.config.js
@@ -8,6 +8,14 @@ module.exports = {
           DEFAULT: '#399AC2',
           dark: '#1C4C6A',
           dark2: '#26476C',
+          focus: '#348CB0'
+        },
+        mygreen: {
+          DEFAULT: '#70BC98',
+          light: '#82D6AE'
+        },
+        mygrey: {
+          DEFAULT: '#6A95C5'
         }
       }
     },
